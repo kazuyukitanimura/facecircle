7ad164d3846c2945e42211570c69a683330897d8
kazuyukitanimura <kazuyukitanimura@gmail.com>
2014-12-07 20:45:45 -0800

    adjust color

--- facecircle/Face.mm
+++ facecircle/Face.mm
@@ -103,16 +103,16 @@
       // G = clip(( 298 * C - 100 * D - 208 * E + 128) >> 8)
       // B = clip(( 298 * C + 516 * D           + 128) >> 8)
       uint32_t t = (i >> 1) * w + (j & -2); // TODO upconvert
-      uint32_t y = yDataAddress[i * w + j];
-      uint32_t u = uvDataAddress[t];
-      uint32_t v = uvDataAddress[t + 1];
+      uint32_t y = yDataAddress[i * w + j]; // 16 - 235
+      uint32_t u = uvDataAddress[t]; // 16 -240
+      uint32_t v = uvDataAddress[t + 1]; // 16 -240
       uint32_t C = y - 16;
       uint32_t D = u - 128;
       uint32_t E = v - 128;
       uint32_t R = 0xFF & (( 298 * C           + 409 * E + 128) >> 8);
       uint32_t G = 0xFF & (( 298 * C - 100 * D - 208 * E + 128) >> 8);
       uint32_t B = 0xFF & (( 298 * C + 516 * D           + 128) >> 8);
-      mat.data[(i + 1) * w - j - 1] = y /*+ (v >> 4) + (u >> 4) ;*/ - ((129 * G) >> 8); // remove R
+      mat.data[(i + 1) * w - j - 1] = C + (R >> 3) - (G >> 7) + (B >> 6); // adjust by color
     }
   }
 
